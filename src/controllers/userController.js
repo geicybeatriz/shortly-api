d7992d0260e5280979e49f7c4e2b01e029471d3a
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-06-08 03:54:35 -0300

    feat: insert user data into database

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,28 @@
 import db from "./../../database/db.js";
+import * as bcrypt from "bcrypt";
 
 export async function addUser(req, res){
-    
+    const {name, email, password, confirm_password} = req.body;
+    const passwordHash = bcrypt.hashSync(password, 10);
+
+    try {
+        const checkUser = await db.query(`
+        SELECT * 
+        FROM users 
+        WHERE email=$1`, [email]);
+        if(checkUser.rows.length !== 0){
+            return res.status(409).send("Este usuário já está cadastrado");
+        }
+
+        await db.query(`
+            INSERT INTO users (name, email, password) 
+            VALUES ($1, $2, $3);`, 
+            [name, email, passwordHash]);
+
+        res.status(201).send("cadastro realizado com sucesso");
+    } catch (error) {
+        console.log("erro", error);
+        res.status(500).send("erro ao cadastrar cliente");
+    }
 }
 
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,8 +1,9 @@
 import { signUpValidate, signInValidate } from "../middlewares/userValidation.js";
+import { addUser } from "../controllers/userController.js";
 import { Router } from "express";
 
 const authController = Router();
-authController.post("/signup", signUpValidate);
+authController.post("/signup", signUpValidate, addUser);
 authController.post("/signin", signInValidate);
 
 export default authController;
