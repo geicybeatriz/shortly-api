1593d6f31113372e9bfced6bef4df0b38b0a310f
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-06-10 08:08:33 -0300

    feat: add getting url by id endpoint

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -4,21 +4,41 @@ import db from "../../database/db.js";
 export async function createUrlShorten(req, res){
     const {user} = res.locals;
     const {url} = req.body;
-    const shortURL = nanoid(8);
-    console.log(shortURL);
-
+    const shortUrl = nanoid(8);
+    
     try {
         await db.query(`
             INSERT INTO urls (url, "shortUrl", "userId") 
             VALUES ($1, $2, $3);`,
-            [url, shortURL, user?.id]);
+            [url, shortUrl, user?.id]);
 
-        res.status(201).send({shortURL});
+        res.status(201).send({shortUrl});
     } catch (error) {
         console.log("erro", error);
         res.sendStatus(500);
     }
 }
+
+export async function getUrlById(req, res){
+    const {id} = req.params;
+    
+    try {
+        const urlsList = await db.query(`
+            SELECT id, "shortUrl", url 
+            FROM urls 
+            WHERE id=$1;`, [id]);
+        console.log("urls",urlsList);
+        const url = urlsList.rows[0];
+        if(urlsList.rowCount === 0) return res.status(404).send("url não encontrada!");
+
+        res.status(200).send(url);
+        
+    } catch (error) {
+        console.log("erro", error);
+        res.sendStatus(500);
+    }
+}
+
 
 
 
--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import { createUrlShorten } from "../controllers/urlsController.js";
+import { createUrlShorten, getUrlById } from "../controllers/urlsController.js";
 import validateSchema from "../middlewares/schemaValidation.js";
 import { tokenValidate } from "../middlewares/tokenValidation.js";
 import urlSchema from "../schemas/urlSchema.js";
 
 const urlController = Router();
 urlController.post("/urls/shorten", tokenValidate, validateSchema(urlSchema), createUrlShorten);
+urlController.get("/urls/:id", getUrlById);
 
 export default urlController;
